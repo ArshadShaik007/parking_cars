c41a1c155280844f8e12fd374400974921f762d3
arshadsmile <arshad007.vip@gmail.com>
2021-01-24 12:09:28 +0530

    after watching a you tube video

--- project/login/models.py
+++ project/login/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class sqlserverconn(models.Model):
+    admin_id = models.IntegerField(max_length=15)
+    username = models.CharField(max_length=45)
+    password = models.CharField(max_length=25)
--- project/login/views.py
+++ project/login/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from models import sqlserverconn
 from datetime import datetime
+import pyodbc
 
 # Create your views here.
+def connsql(request):
+    conn=pyodbc.connect('Driver={sqlserver};Server=ARCHANGEL\SQLEXPRESS;Database=parking;Trusted_Connection=yes;')
+    cursor=conn.cursor()
+    cursor.execute("select * from vehicle_table")
 def login_page(request):
     page_name="login page is here"
     return render(request, 'login_page.html', {'date':datetime.now()})
